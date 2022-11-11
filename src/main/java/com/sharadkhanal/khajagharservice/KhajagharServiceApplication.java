d1051c156769da8862066927cb04a5b1f756becc
SharadKhanal <khanelsharad@gmail.com>
2022-11-11 15:38:20 +0545

    initialize Commit

---
+++ src/main/java/com/sharadkhanal/khajagharservice/KhajagharServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sharadkhanal.khajagharservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhajagharServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhajagharServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharadkhanal/khajagharservice/KhajagharServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharadkhanal.khajagharservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhajagharServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
